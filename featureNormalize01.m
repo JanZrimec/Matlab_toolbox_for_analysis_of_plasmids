91500e901d16b72818d6fef8161da6e03ceb55a4
JanZrimec <37239764+JanZrimec@users.noreply.github.com>
2018-03-12 22:17:56 +0100

    Delete featureNormalize01.m

--- featureNormalize01.m
+++
@@ -1,7 +0,0 @@
-function [X_norm] = featureNormalize01(X,minx,maxx)
-%FEATURENORMALIZE Normalizes the features in X 
-
-[m,n] = size(X);
-X_norm = (X-repmat(minx,m,n))./(repmat(maxx-minx,m,n));
-
-end
